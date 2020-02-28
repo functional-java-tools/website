22d5020422eaa0a7e85386c0a39530bedb3a599b
Pablo AM <>
2020-02-28 19:36:30 +0100

    Vue+Nuxt+TS with prettier & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports =  {
+  parser:  'vue-eslint-parser',  // Specifies the ESLint parser
+  extends:  [
+    'plugin:vue/recommended',
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'prettier/vue',
+    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+ parserOptions:  {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+    sourceType:  'module',  // Allows for the use of imports
+  },
+  rules:  {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+};
