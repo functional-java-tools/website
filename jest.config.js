9f3414e3351e0bb19b442b595d5a26fa5ed0c7ae
Pablo AM <pablo.alv.men@gmail.com>
2022-07-12 17:36:20 +0200

    Feature/vue+nuxt+ts (#1)
    * Vue+Nuxt+TS with prettier & eslint
    
    * Added logic as TS
    
    * Added jest with typescript
    
    Co-authored-by: Pablo AM <>

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1',
+        '^~/(.*)$': '<rootDir>/$1',
+        '^vue$': 'vue/dist/vue.common.js',
+    },
+    moduleFileExtensions: ['js', 'vue', 'json', 'ts'],
+    transform: {
+        '^.+\\.js$': 'babel-jest',
+        '.*\\.(vue)$': 'vue-jest',
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    collectCoverage: true,
+    collectCoverageFrom: ['<rootDir>/components/**/*.vue', '<rootDir>/pages/**/*.vue'],
+};
